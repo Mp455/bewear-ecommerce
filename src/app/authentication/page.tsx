ce0a54c9440b5d8fdbd9d680124d9324f54e4ce6
Marcos Paulo <marcospaulosousa48@gmail.com>
2025-08-08 10:48:53 -0300

    feat: add header to sign and signout

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignForm from "./components/sign-in-form";
@@ -5,19 +6,22 @@ import SignupForm from "./components/sign-up-form";
 
 export default function AuthenticationPage() {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="sign-in">
-          <SignForm />
-        </TabsContent>
-        <TabsContent value="sign-up">
-          <SignupForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <Header />
+      <div className="flex w-full flex-col gap-6 p-5">
+        <Tabs defaultValue="sign-in">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="sign-in">
+            <SignForm />
+          </TabsContent>
+          <TabsContent value="sign-up">
+            <SignupForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 }
